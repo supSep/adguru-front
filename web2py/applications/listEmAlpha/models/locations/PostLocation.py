7abc9ac1bd247607d0ac94087a2d8d57d101f5b3
Sep Taheri <sept@uvic.ca>
2014-10-13 20:48:42 -0700

    Added location objects

--- web2py/applications/listEmAlpha/models/locations/PostLocation.py
+++ web2py/applications/listEmAlpha/models/locations/PostLocation.py
@@ -1,30 +1,33 @@
 from enum import Enum
+from applications.listEmAlpha.models.locations import CraigslistLocation
+from applications.listEmAlpha.models.locations.KijLocation import KijLocation
+
 
 class PostLocation(Enum):
-	burnaby_newwest = (
-		CraigslistLocation.burnaby_newwest,
-		 KijLocation.burnaby_newwest)
+    burnaby_newwest = (
+        CraigslistLocation.burnaby_newwest.value,
+        KijLocation.burnaby_newwest.value)
 
-	delta_surrey_langley = (
-		CraigslistLocation.delta_surrey_langley,
-		 KijLocation.delta_surrey_langley)
+    delta_surrey_langley = (
+        CraigslistLocation.delta_surrey_langley.value,
+        KijLocation.delta_surrey_langley.value)
 
-	northshore = (
-		CraigslistLocation.northshore,
-		 KijLocation.northshore)
+    northshore = (
+        CraigslistLocation.northshore.value,
+        KijLocation.northshore.value)
 
-	richmond = (
-		CraigslistLocation.richmond,
-		 KijLocation.richmond)
+    richmond = (
+        CraigslistLocation.richmond.value,
+        KijLocation.richmond.value)
 
-	ubc = (
-		CraigslistLocation.vancouver,
-		 KijLocation.ubc)
+    ubc = (
+        CraigslistLocation.vancouver.value,
+        KijLocation.ubc.value)
 
-	vancouver = (
-		CraigslistLocation.vancouver,
-		 KijLocation.vancouver)
+    vancouver = (
+        CraigslistLocation.vancouver.value,
+        KijLocation.vancouver.value)
 
-	tricities_pitt_maple = (
-		CraigslistLocation.tricities_pitt_maple,
-		 KijLocation.tricities_pitt_maple)
+    tricities_pitt_maple = (
+        CraigslistLocation.tricities_pitt_maple.value,
+        KijLocation.tricities_pitt_maple.value)
